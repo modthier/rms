5a004f0afdfcd8ab31632bae954fb6bf11df38b2
modthier <modthier47387@live.com>
2023-12-13 16:06:18 +0200

    added orderType

--- app/Models/OrderType.php
+++ app/Models/OrderType.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class OrderType extends Model
 {
     use HasFactory;
+
+    public function order()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
